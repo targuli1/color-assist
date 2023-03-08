306594c0bcc17d9b6f8e0dee458709b55236c43a
targuli1 <umid.targuliyev@mail.utoronto.ca>
2023-03-08 10:07:51 +0400

    Add simulation for text color

--- business-logic/finder.js
+++ business-logic/finder.js
@@ -4,24 +4,35 @@ function grabAllColors() {
     const elements = document.getElementsByTagName("*") ?? []
     console.log("Total amount of div refs: ", elements.length)
     var colorMap = {}
-    var binder = {}
-    var count = 0
+    var backgroundColorMap = {}
+    var bgCount = 0
+    var txtCount = 0
 
     for (var i = 0; i < elements.length; i += 1) {
-        const element_color = getComputedStyle(elements[i]).backgroundColor.toString()
-        
-        if (binder[element_color] == undefined) {
-            binder[element_color] = "bind_" + count
-            count += 1
+        const bg_color = getComputedStyle(elements[i]).backgroundColor.toString()
+        const text_color = getComputedStyle(elements[i]).color.toString()
+        if (backgroundColorMap[bg_color] == undefined) {
+            backgroundColorMap[bg_color] = "background-color-binder-" + bgCount;
+            bgCount += 1
+        }
+        if (! elements[i].classList.contains(backgroundColorMap[bg_color])) {
+            elements[i].className += " " + backgroundColorMap[bg_color]
         }
 
-        if (! elements[i].classList.contains(binder[element_color])) {
-            elements[i].className += " " + binder[element_color]
+
+        if (colorMap[text_color] == undefined) {
+            colorMap[text_color] = "text-color-binder-" + txtCount;
+            txtCount += 1
         }
 
+        if (!elements[i].classList.contains(colorMap[text_color])) {
+            elements[i].className += " " + colorMap[text_color]
+        }
     }
 
-    chrome.storage.local.set({"map": binder})
+    chrome.storage.local.set({"background-color-map": backgroundColorMap})
+    chrome.storage.local.set({"text-color-map": colorMap})
+
     chrome.storage.local.set({"status": "finder-end"})
 }
 
--- business-logic/simulater.js
+++ business-logic/simulater.js
@@ -35,49 +35,70 @@ function applyMatrix(rgb_list, matrix) {
 }
 
 
+function changeColor(mapName, propertyName) {
+    chrome.storage.local.get(mapName, (items)=> {
+        console.log("DEBUG: ", items)
 
-function changeColor() {
-     
-   
-    // import deuter from "./conversion.js"
-
-    chrome.storage.local.get("map", (items)=> {
-
-        
         chrome.storage.local.get("simulation-type", (type) => {
-            const target_keys =  Object.keys(items.map)    
-            console.log("DEBUGGGG: ", type["simulation-type"])
+            const colorMap = items[mapName] ?? []
+            const target_keys =  Object.keys(colorMap)    
             let applied_matrix = _colormap[type["simulation-type"]]
-            console.log("last")
 
             for (var ti = 0; ti < target_keys.length; ti += 1) {
-                const target = items.map[target_keys[ti]]
+                const target = colorMap[target_keys[ti]]
                 
                 const rgb = findRGB(target_keys[ti])
 
                 const nrgb = applyMatrix(rgb, applied_matrix)
 
-                console.log(rgb, "vs", nrgb)
                 const elements = document.getElementsByClassName(target)
                 for (var i = 0; i < elements.length; i+=1) {
-                    if (rgb.length == 3) {
-                        const color = "rgb(" + nrgb[0] + ", " + nrgb[1] + ", " + nrgb[2] + ")"
-                        elements[i].style.setProperty("background-color", color, "important")
-                        // elements[i].style.backgroundColor = color
-                    }
-                    else {
-                        const color = "rgba(" + nrgb[0] + ", " + nrgb[1] + ", " + nrgb[2] + ", " + rgb[3] + ")"
-                        elements[i].style.setProperty("background-color", color, "important")
-
-                        // elements[i].style.backgroundColor = color
-                    }
+                    let color;
+                    if (!rgb[0] && !rgb[1] & !rgb[2]) continue;
+                    if (rgb.length == 3) color = "rgb(" + nrgb[0] + ", " + nrgb[1] + ", " + nrgb[2] + ")"
+                    else color = "rgba(" + nrgb[0] + ", " + nrgb[1] + ", " + nrgb[2] + ", " + rgb[3] + ")"                    
+                    elements[i].style.setProperty(propertyName, color, "important")
                 }
             }
         })
 
-        
-
     })
+}
+
+
+function simulate() {
+     
+   
+    // import deuter from "./conversion.js"
+
+    changeColor("background-color-map", "background-color");
+    changeColor("text-color-map", "color");
+    // chrome.storage.local.get("background-color-map", (items)=> {
+    //     console.log("DEBUG: ", items)
+
+    //     chrome.storage.local.get("simulation-type", (type) => {
+    //         const backgroundColorMap = items["background-color-map"] ?? []
+    //         const target_keys =  Object.keys(backgroundColorMap)    
+    //         let applied_matrix = _colormap[type["simulation-type"]]
+
+    //         for (var ti = 0; ti < target_keys.length; ti += 1) {
+    //             const target = backgroundColorMap[target_keys[ti]]
+                
+    //             const rgb = findRGB(target_keys[ti])
+
+    //             const nrgb = applyMatrix(rgb, applied_matrix)
+
+    //             const elements = document.getElementsByClassName(target)
+    //             for (var i = 0; i < elements.length; i+=1) {
+    //                 let color;
+    //                 if (rgb.length == 3) color = "rgb(" + nrgb[0] + ", " + nrgb[1] + ", " + nrgb[2] + ")"
+    //                 else color = "rgba(" + nrgb[0] + ", " + nrgb[1] + ", " + nrgb[2] + ", " + rgb[3] + ")"                    
+    //                 elements[i].style.setProperty("background-color", color, "important")
+    //             }
+    //         }
+    //     })
+
+    // })
 }
 
 
@@ -110,7 +131,7 @@ chrome.runtime.sendMessage({msg: "status"}, async function (response) {
     console.log(response)
     if (response.data.status == "simulater-start") {
         console.log("Simulater trigger: On")
-        changeColor()
+        simulate()
         chrome.storage.local.set({"status": "simulater-end"})
         
     }
